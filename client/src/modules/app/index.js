05c5ee4ef748ac9233731aa65d52be931c727149
yuki <yukisanada1992@gmail.com>
2019-12-18 23:27:10 +1000

    add topbar

--- client/src/modules/app/index.js
+++ client/src/modules/app/index.js
@@ -4,11 +4,12 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import AddUser from './Users/AddUser';
 import EditUser from './Users/EditUser';
 import UserList from './Users/UserList';
-
+import TopBar from '../../components/TopBar';
 class App extends Component {
   render() {
     return (
       <div className="auth-app">
+        <TopBar />
         <Switch>
           <Route exact path="/users" component={UserList} />
           <Route exact path="/users/edit/:id" component={EditUser} />
